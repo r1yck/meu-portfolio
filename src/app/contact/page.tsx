be646992001071bc677f0c8dd35bed41af3dfdc7
Henrique Fontoura <henriquefontoura.exe@gmail.com>
2025-02-03 16:23:26 -0300

    update: contactPage e inputsComponent (adicionado api emailJS)

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -1,14 +1,51 @@
+"use client";
+
+import React, { useState } from "react";
+import emailjs from "@emailjs/browser";
 import Header from "@/components/Header";
 import Footer from "@/components/Footer";
-import Input from "@/components/Inputs"; 
+import Input from "@/components/Inputs";
 
 export default function ContactPage() {
+    const [formData, setFormData] = useState({
+        from_name: "",
+        from_email: "",
+        subject: "",
+        message: "",
+    });
+
+    const handleChange = (e: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>) => {
+        setFormData({ ...formData, [e.target.name]: e.target.value });
+    };
+
+    const handleSubmit = async (e: React.FormEvent) => {
+        e.preventDefault();
+
+        try {
+            await emailjs.send(
+                process.env.NEXT_PUBLIC_EMAILJS_SERVICE_ID!,
+                process.env.NEXT_PUBLIC_EMAILJS_TEMPLATE_ID!,
+                {
+                    from_name: formData.from_name,
+                    from_email: formData.from_email,
+                    subject: formData.subject,
+                    message: formData.message,
+                },
+                process.env.NEXT_PUBLIC_EMAILJS_PUBLIC_KEY!
+            );
+
+
+            alert("Mensagem enviada com sucesso!");
+            setFormData({ from_name: "", from_email: "", subject: "", message: "" });
+        } catch (error) {
+            console.error("Erro ao enviar mensagem:", error);
+            alert("Ocorreu um erro ao enviar a mensagem.");
+        }
+    };
+
     return (
         <>
-            <Header
-                title="Contato"
-                subtitle="Saiba como entrar em contato comigo"
-            />
+            <Header title="Contato" subtitle="Saiba como entrar em contato comigo" />
             <section className="p-8 flex flex-wrap justify-between items-start gap-8 max-w-[1200px] mx-auto">
                 {/* Redes Sociais */}
                 <div className="text-textBlue w-full md:w-[45%] lg:w-[40%]">
@@ -16,34 +53,19 @@ export default function ContactPage() {
                     <ul className="space-y-4">
                         <li className="flex items-center">
                             <img src="/assets/ig.png" alt="Instagram" className="w-8 h-8 mr-4" />
-                            <a
-                                href="https://www.instagram.com/r1yck/"
-                                target="_blank"
-                                rel="noopener noreferrer"
-                                className="text-white underline hover:text-textBlue"
-                            >
+                            <a href="https://www.instagram.com/r1yck/" target="_blank" rel="noopener noreferrer" className="text-white underline hover:text-textBlue">
                                 Instagram
                             </a>
                         </li>
                         <li className="flex items-center">
                             <img src="/assets/wpp.png" alt="Whatsapp" className="w-8 h-8 mr-4" />
-                            <a
-                                href="https://wa.me/5573998336602"
-                                target="_blank"
-                                rel="noopener noreferrer"
-                                className="text-white underline hover:text-textBlue"
-                            >
+                            <a href="https://wa.me/5573998336602" target="_blank" rel="noopener noreferrer" className="text-white underline hover:text-textBlue">
                                 Whatsapp
                             </a>
                         </li>
                         <li className="flex items-center">
                             <img src="/assets/github.png" alt="Github" className="w-8 h-8 mr-4" />
-                            <a
-                                href="https://github.com/r1yck"
-                                target="_blank"
-                                rel="noopener noreferrer"
-                                className="text-white underline hover:text-textBlue"
-                            >
+                            <a href="https://github.com/r1yck" target="_blank" rel="noopener noreferrer" className="text-white underline hover:text-textBlue">
                                 Github
                             </a>
                         </li>
@@ -52,22 +74,38 @@ export default function ContactPage() {
 
                 {/* Formulário de Contato */}
                 <div className="w-full md:w-[45%] lg:w-[40%]">
-                    <h2 className="text-2xl font-bold text-textBlue mb-4 text-center md:text-left">
-                        Envie uma mensagem:
-                    </h2>
-                    <form
-                        action="https://formsubmit.co/henriquefontoura.exe@gmail.com"
-                        method="POST"
-                        className="flex flex-col gap-4"
-                    >
-                        <Input placeholder="Digite seu nome e sobrenome" />
-                        <Input placeholder="Digite seu e-mail" />
-                        <Input placeholder="Digite o assunto" />
-                        <Input placeholder="Sua mensagem..." isLarge />
-                        <button
-                            type="submit"
-                            className="w-full bg-textBlue text-white py-2 px-4 rounded-lg font-bold hover:bg-blue-600"
-                        >
+                    <h2 className="text-2xl font-bold text-textBlue mb-4 text-center md:text-left">Envie uma mensagem:</h2>
+                    <form onSubmit={handleSubmit} className="flex flex-col gap-4">
+                        <Input
+                            type="text"
+                            name="from_name"
+                            value={formData.from_name}
+                            onChange={handleChange}
+                            placeholder="Digite seu nome e sobrenome"
+                            required />
+                        <Input
+                        type="email"
+                            name="from_email"
+                            value={formData.from_email}
+                            onChange={handleChange}
+                            placeholder="Digite seu e-mail"
+                            required />
+                        <Input
+                            type="text"
+                            name="subject"
+                            value={formData.subject}
+                            onChange={handleChange}
+                            placeholder="Digite o assunto"
+                            required />
+                        <Input
+                            type="text"
+                            name="message"
+                            value={formData.message}
+                            onChange={handleChange}
+                            placeholder="Sua mensagem..."
+                            isLarge
+                            required />
+                        <button type="submit" className="w-full bg-textBlue text-white py-2 px-4 rounded-lg font-bold hover:bg-blue-600">
                             Enviar
                         </button>
                     </form>
--- src/components/Inputs.tsx
+++ src/components/Inputs.tsx
@@ -3,14 +3,31 @@ import React from "react";
 interface InputProps {
   placeholder: string;
   isLarge?: boolean;
+  name: string;
+  value: string;
+  onChange: (e: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>) => void;
+  required?: boolean;
+  type?: string; 
 }
 
-export default function Input({ placeholder, isLarge }: InputProps) {
+export default function Input({
+  placeholder,
+  isLarge,
+  name,
+  value,
+  onChange,
+  required,
+  type = "text", 
+}: InputProps) {
   return (
     <input
-      type="text"
+      type={type} 
+      name={name}
+      value={value}
+      onChange={onChange}
       placeholder={placeholder}
-      className={`box-border bg-white border-4 border-[#4C70EF] text-black text-base p-3 w-full ${
+      required={required}
+      className={`box-border bg-white border-4 border-textBlue text-black text-base p-3 w-full ${
         isLarge ? "h-[394px]" : "h-[53px]"
       }`}
     />
